0c7853d4a0c1a0f3b69b2a0f7de49fc8e0831f16
h668805 <668805@stud.hvl.no>
2024-09-02 13:44:35 +0200

    k

--- f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks1reduce.java
+++ f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks1reduce.java
@@ -7,8 +7,13 @@ public class Eks1reduce {
 	public static void main(String[] args) {
 		
 		/* Summen av aldrene til personene i people-listen */
+		int summen = people.stream().mapToInt(Person::age).sum();
+		System.out.println(summen);
 		
 		/* En streng med alle initialene, "CD LC TC CB MA" */
-
+		String s = people.stream().map(p -> "" + p.firstName().charAt(0)
+				+ p.lastName().charAt(0))
+				.reduce("", (akk, x) -> akk + x + " ");
+		System.out.println(s);
 	}	
 }
--- f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks2countmatch.java
+++ f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks2countmatch.java
@@ -7,12 +7,20 @@ public class Eks2countmatch {
 	public static void main(String[] args) {
 
 		//Alle forbokstavene i fornavnene i en streng "CLTCM" - reduce() 
+		String forbokstaver = people.stream().map(Person::firstName).reduce("", (akku, s) -> akku + s.charAt(0));
+		System.out.println(forbokstaver);
 		
 		//Antall personer over 50 år - count()
-		
+		long antall = people.stream().filter(p -> p.age() > 50).count();
+		System.out.println(antall);
+				
 		//Om vi har data som matcher - anyMatch(), allMatch(), noneMatch()
 		//Er alle over 30 år?
+		boolean over30 = people.stream().allMatch(p -> p.age() > 30);
+		System.out.println(over30);
 		//Er noen over 60 år?
+		boolean over60 = people.stream().anyMatch(p -> p.age() > 60);
+		System.out.println(over60);
 
 	}
 
